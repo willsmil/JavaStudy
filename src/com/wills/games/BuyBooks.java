cf2e88b33eaab30984d1ff2e80bd669d513eed88
willsmil <18240440420@163.com>
2017-09-07 22:02:17 +0800

    create package

---
+++ src/com/wills/games/BuyBooks.java
@@ -0,0 +1,8 @@
+package com.wills.games;
+
+/**
+ * on 17-8-28
+ * created by wills
+ */
+public class BuyBooks {
+}
---
+++ src/com/wills/nowcoder/Choir.java
@@ -0,0 +1,8 @@
+package com.wills.nowcoder;
+
+/**
+ * on 17-8-28
+ * created by wills
+ */
+public class Choir {
+}
