263367b87f0d2f7271365413afe57abc4261cbdc
wills <18240440420@163.com>
2018-04-01 18:18:13 +0800

    -

---
+++ src/com/wills/xlsx/xlsx.java
@@ -0,0 +1,54 @@
+package com.wills.xlsx;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+/**
+ * Created by 18240 on 2018/3/25.
+ */
+public class xlsx {
+    public static List<String[]> readExcel(MultipartFile file) throws IOException {
+        //检查文件
+        checkFile(file);
+        //获得Workbook工作薄对象
+        Workbook workbook = getWorkBook(file);
+        //创建返回对象，把每行中的值作为一个数组，所有行作为一个集合返回
+        List<String[]> list = new ArrayList<String[]>();
+        if(workbook != null){
+            for(int sheetNum = 0;sheetNum < workbook.getNumberOfSheets();sheetNum++){
+                //获得当前sheet工作表
+                Sheet sheet = workbook.getSheetAt(sheetNum);
+                if(sheet == null){
+                    continue;
+                }
+                //获得当前sheet的开始行
+                int firstRowNum  = sheet.getFirstRowNum();
+                //获得当前sheet的结束行
+                int lastRowNum = sheet.getLastRowNum();
+                //循环除了第一行的所有行
+                for(int rowNum = firstRowNum+1;rowNum <= lastRowNum;rowNum++){
+                    //获得当前行
+                    Row row = sheet.getRow(rowNum);
+                    if(row == null){
+                        continue;
+                    }
+                    //获得当前行的开始列
+                    int firstCellNum = row.getFirstCellNum();
+                    //获得当前行的列数
+                    int lastCellNum = row.getPhysicalNumberOfCells();
+                    String[] cells = new String[row.getPhysicalNumberOfCells()];
+                    //循环当前行
+                    for(int cellNum = firstCellNum; cellNum < lastCellNum;cellNum++){
+                        Cell cell = row.getCell(cellNum);
+                        cells[cellNum] = getCellValue(cell);
+                    }
+                    list.add(cells);
+                }
+            }
+            workbook.close();
+        }
+        return list;
+    }
+}
